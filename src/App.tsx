4badf16e8fb91ceb77ce229e43ce433c8b774611
Al Samir <shahinur1591@gmail.com>
2024-05-17 20:27:38 +0600

    Banner done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Outlet />
       <Footer />
     </>
